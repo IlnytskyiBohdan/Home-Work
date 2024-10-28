b2570fe24934927daa5daad9f91d78f1c06ac287
IlnytskyiBohdan <neklz1@icloud.com>
2024-10-28 23:00:45 +0200

    dz 11

--- dz 11 24.10.24/main.js
+++ dz 11 24.10.24/main.js
@@ -1,13 +1,7 @@
 //  DZ 11.1
 
 function createPifagorasTable(size) {
-  const container = document.createElement("div");
-  container.style.display = "flex";
-  container.style.justifyContent = "center";
- 
-
   const table = document.createElement("table");
-  table.style.borderCollapse = "collapse";
 
   for (let i = 1; i <= size; i++) {
     const row = document.createElement("tr");
@@ -15,50 +9,26 @@ function createPifagorasTable(size) {
     for (let j = 1; j <= size; j++) {
       const cell = document.createElement("td");
       cell.textContent = i * j;
-      cell.style.border = "1px solid black";
-      cell.style.padding = "5px";
-      cell.style.textAlign = "center";
-
       row.appendChild(cell);
     }
 
     table.appendChild(row);
   }
 
-  container.appendChild(table);
-  document.body.appendChild(container);
+  document.getElementById("table-container").appendChild(table);
 }
 
 createPifagorasTable(10);
-
-
-
-
-
-
 
 //  DZ 11.2
 
 const textBlock = document.getElementById("textBlock");
 const button = document.getElementById("colorToggleButton");
 
-const originalColor = "black";
-const newColor = "red";
-let isOriginalColor = true;
-
 button.addEventListener("click", function () {
-  if (isOriginalColor) {
-    textBlock.style.color = newColor;
-  } else {
-    textBlock.style.color = originalColor;
-  }
-  isOriginalColor = !isOriginalColor;
+  textBlock.classList.toggle("newColor");
+  textBlock.classList.toggle("originalColor");
 });
-
-
-
-
-
 
 //  DZ 11.3
 
@@ -78,10 +48,6 @@ const images = [
 
 const randomIndex = Math.floor(Math.random() * images.length);
 const randomImage = images[randomIndex];
-
 const imageElement = document.getElementById("randomImage");
-if (imageElement) {
-  imageElement.src = `${imageFolderPath}/${randomImage}`;
-} else {
-  console.error('Елемент з id="randomImage" не знайдено в HTML');
-}
+
+imageElement.src = `${imageFolderPath}/${randomImage}`;

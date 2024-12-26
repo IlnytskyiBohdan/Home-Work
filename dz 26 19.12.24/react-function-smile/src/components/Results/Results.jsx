c0a83d2922f23aa9f2a18907a8f7c44db49e76cd
IlnytskyiBohdan <neklz1@icloud.com>
2024-12-26 11:28:07 +0200

    dz 26

--- dz 26 19.12.24/react-function-smile/src/components/Results/Results.jsx
+++ dz 26 19.12.24/react-function-smile/src/components/Results/Results.jsx
@@ -2,17 +2,18 @@ import "./Results.css";
 import PropTypes from "prop-types";
 
 const Results = ({ winner, emojis, votes }) => {
+  if (winner === null) return null;
+
   return (
-    winner && (
-      <div className="results-container">
-        <h2 className="results-title">Результати голосування:</h2>
-        <p>Переможець:</p>
-        <div className="winner-emoji">{emojis[winner]}</div>
-        <p>Кількість голосів: {votes[winner]}</p>
-      </div>
-    )
+    <div className="results-container">
+      <h2 className="results-title">Результати голосування:</h2>
+      <p>Переможець:</p>
+      <div className="winner-emoji">{emojis[winner]}</div>
+      <p>Кількість голосів: {votes[winner]}</p>
+    </div>
   );
 };
+
 Results.propTypes = {
   winner: PropTypes.number,
   emojis: PropTypes.arrayOf(PropTypes.string).isRequired,
